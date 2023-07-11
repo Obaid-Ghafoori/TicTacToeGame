2bfbe92eac91ab5e301247acd4ab62561fcad917
Obaid Ghafoori <43299028+Obaid-Ghafoori@users.noreply.github.com>
2023-07-11 19:33:29 +0200

    refactor: refactor the code

--- src/main/java/com/obaid/TicTacToeGame/TicTacToeGameApplication.java
+++ src/main/java/com/obaid/TicTacToeGame/TicTacToeGameApplication.java
@@ -10,14 +10,16 @@ public class TicTacToeGameApplication {
     private static char SYMBOL;
     private static int playerPos;
     private static Scanner input;
-    private static char[][] gameBoard;
     private static int computerPos;
+    private static char[][] gameBoard;
+    private static boolean isRunning = false;
     private static ArrayList<Integer> playerPositions = new ArrayList<>();
     private static ArrayList<Integer> computerPositions = new ArrayList<>();
 
     public static void main(String[] args) {
         SpringApplication.run(TicTacToeGameApplication.class, args);
         makeGameBoard();
+        printGameBoard(gameBoard);
         runGame(gameBoard);
     }
 
@@ -29,62 +31,55 @@ public class TicTacToeGameApplication {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}
         };
-        printGameBoard(gameBoard);
-
     }
 
     public static void printGameBoard(char[][] gameBoard) {
         for (char[] row : gameBoard) {
-            for (char cel : row) {
-                System.out.print(cel);
+            for (char mark : row) {
+                System.out.print(mark);
             }
             System.out.println();
         }
     }
 
     public static void runGame(char[][] gameBoard) {
-        while (true) {
+        isRunning = true;
+        while (isRunning) {
             // player enter a number
             input = new Scanner(System.in);
             System.out.println("Enter number from 1 to 9");
             playerPos = input.nextInt();
-
-            while (playerPositions.contains(playerPos) || computerPositions.contains(playerPositions) || computerPos == playerPos) {
-                System.out.println("this position is taken, try it in other free spot!");
-                playerPos = input.nextInt();
-            }
-
-            playYourTurn(gameBoard, playerPos, "player");
-
-            String result = checkWinner();
-            if (result.length() > 0) {
-                System.out.println(result);
-                break;
-            }
+            playPlayer();
+            printWinnerMessage();
 
             // let the computer chose a random number
-            Random randomizer = new Random();
-            computerPos = randomizer.nextInt(9) + 1;
-
-            while (playerPositions.contains(computerPos) || computerPositions.contains(computerPos) || computerPos == playerPos) {
-                computerPos = randomizer.nextInt(9) + 1;
-            }
-
-            playYourTurn(gameBoard, computerPos, "computer");
+            playComputer();
             printGameBoard(gameBoard);
+            printWinnerMessage();
+        }
+    }
 
-            result = checkWinner();
-            if (result.length() > 0) {
-                System.out.println(result);
-                break;
-            }
+    public static void playComputer() {
+        // let the computer chose a random number
+        Random randomizer = new Random();
+        computerPos = randomizer.nextInt(9) + 1;
 
+        while (playerPositions.contains(computerPos) || computerPositions.contains(computerPos) || computerPos == playerPos) {
+            computerPos = randomizer.nextInt(9) + 1;
         }
 
+        playYourTurn(gameBoard, computerPos, "computer");
+    }
+
+    public static void playPlayer() {
+        while (playerPositions.contains(playerPos) || computerPositions.contains(playerPositions) || computerPos == playerPos) {
+            System.out.println("this position is taken, try it in other free spot!");
+            playerPos = input.nextInt();
+        }
+        playYourTurn(gameBoard, playerPos, "player");
     }
 
     public static void playYourTurn(char[][] gameBoard, Integer position, String player) {
-
         if (player.equals("player")) {
             SYMBOL = 'X';
             playerPositions.add(position);
@@ -159,5 +154,13 @@ public class TicTacToeGameApplication {
             }
         }
         return result;
+    }
+
+    public static void printWinnerMessage() {
+        String result = checkWinner();
+        if (result.length() > 0) {
+            System.out.println(result);
+            isRunning = false;
+        }
     }
 }
